3576aa009389f93840ba5e05b4cdbe9c780d203f
Cartoonish Villain <jedijoe93@gmail.com>
2022-04-10 16:46:24 -0400

    Add comments, fix numerical errors, close more input lines

--- src/com/villain/Main.java
+++ src/com/villain/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Prompt the user for their preferred operation method.
         modeSelect();
 
         try {
@@ -22,8 +23,10 @@ public class Main {
             ioe.printStackTrace();
             System.out.println("ioException in input phase");
         }
+        //Selection phase. Go through each prize and randomly pick a winner, until there are either no more prizes or participants.
         selectWinners();
 
+        //Output phase. Take the selected data, and convert it to a text file, and post it in logs.
         try {
             writeOutput();
         } catch (IOException ioe) {
@@ -39,10 +42,12 @@ public class Main {
         System.out.println("1: Utilize exact numbers (Eg: 7.6 entries -> 7.6 entries)");
         System.out.println("2: Standard rounding: (Eg: 7.4 entries -> 7 entries, 7.5 entries -> 8 entries)");
         Scanner scanner = new Scanner(System.in);
+        //Loop until the mode is set properly
         while (mode == -1) {
             String input = scanner.nextLine();
             int value;
 
+            //If the value is invalid, reset the loop.
             try {
                 value = Integer.parseInt(input);
             } catch (NumberFormatException e) {
@@ -50,7 +55,7 @@ public class Main {
                 continue;
             }
 
-            //If the value is 0-2, asign the value to break from the loop, otherwise continue going through and looping.
+            //If the value is 0-2, assign the value to break from the loop, otherwise continue going through and looping.
             switch (value) {
                 case 0:
                 case 1:
@@ -65,6 +70,7 @@ public class Main {
     }
 
     public static void setUpPrizes() throws IOException {
+        //Open prizes.txt if present
         FileInputStream in;
         try {
             in = new FileInputStream("prizes.txt");
@@ -73,18 +79,21 @@ public class Main {
             return;
         }
 
+        //Prepare a line by line reader.
         BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
 
+        //Loop through each line of prizes.txt, and assign them as prizes in order.
         String line;
         while ((line = reader.readLine()) != null) {
             prizes.add(line);
         }
         in.close();
+        reader.close();
     }
 
     public static void setUpPool() throws IOException {
+        //Open data.csv if present
         FileInputStream in;
-
         try {
             in = new FileInputStream("data.csv");
         } catch (FileNotFoundException e) {
@@ -92,7 +101,10 @@ public class Main {
             return;
         }
 
+        //Prepare a line by line reader.
         BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+        //Calibration phase. Read the first line of the csv to get bearings
         String line = null;
         try {
             line = reader.readLine();
@@ -100,35 +112,37 @@ public class Main {
             e.printStackTrace();
         }
 
-        //get column ids.
+        //Find the columns needed to work, and mark them as indices.
         int uuidIndex = -1;
         String[] tokens = line.split(","); //split first line into the columns
         int counter = 0;
         for (String content : tokens) {
-            String truecontent = content.replaceAll("[-+^.:,\"]", ""); //some formats do weird stuff with extra characters. This helps resolve the issue.
+            String truecontent = content.replaceAll("[-+^.:,\"]", ""); //some formats do weird stuff with extra characters.
+            //"player" refers to a UUID or username based on example data. Code here could be edited to match the file, or the file could be edited to match the code.
             if (truecontent.equalsIgnoreCase("player")) {
-                uuidIndex = counter; //if the date column is found, mark it's index for future imports
+                uuidIndex = counter; //if the player column is found, mark its index for future imports
                 break;
             }
             counter++;
         }
 
-        int entryIndex = -1; //the general process listed above for the date is followed
+        int entryIndex = -1; //the general process listed above for the players is followed for the entry count as well.
         counter = 0;
         for (String content : tokens) {
             String truecontent = content.replaceAll("[-+^.:,\"]", "");
-            if (truecontent.equalsIgnoreCase("entries")) {
+            if (truecontent.equalsIgnoreCase("entries")) { //"entries" is again, entered here as a reference to example data.
                 entryIndex = counter;
                 break;
             }
             counter++;
         }
 
-        if (uuidIndex == -1 || entryIndex == -1) {
+        if (uuidIndex == -1 || entryIndex == -1) { // if either index is missing, stop.
             System.out.println("CSV Index table does not contain necessary values! Need a player and entries column");
+            return;
         }
 
-        while ((line = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) { //Go through each line of the data.csv file, indexes in hand, and add each entry to the map.
             tokens = line.split(",");
             if (!tokens[0].equals("")) {
                 float value = Float.parseFloat(tokens[entryIndex]);
@@ -136,17 +150,18 @@ public class Main {
             }
         }
 
-        in.close();
+        in.close(); //close input.
+        reader.close();
     }
 
     public static Float modeMath(Float value) {
         switch (mode) {
             case 0:
-                return (float) value.intValue();
+                return (float) value.intValue(); //Mode 0 returns the int value of the float
             case 1:
-                return value;
+                return value; //Mode 1 uses the float as is
             case 2:
-                float decimal = value - (float) value.intValue();
+                float decimal = value - (float) value.intValue(); //Mode 2 uses the int value, or int value plus one of the float depending on the decimal.
                 if (decimal < 0.5f) {
                     return (float) value.intValue();
                 } else {
@@ -158,29 +173,31 @@ public class Main {
     }
 
     public static void selectWinners() {
-        while (pool.size() > 0 && winners.size() < prizes.size()) {
-            int Total = 0;
-            for (Map.Entry<String, Float> entry : pool.entrySet()) {
+        Random random = new Random();
+        while (pool.size() > 0 && winners.size() < prizes.size()) { //while we still have players in the pool, and we have less winners than prizes...
+            float Total = 0;
+            for (Map.Entry<String, Float> entry : pool.entrySet()) { //Loop through every player in the pool, and add their entry values together
                 Total += entry.getValue();
             }
-            int random = new Random().nextInt(Total);
+            float randomval = random.nextFloat() * Total; //Generate a random number between 0 and the combined total of all
             String winner = "";
             float chance = 0f;
-            for (Map.Entry<String, Float> entry : pool.entrySet()) {
-                random -= entry.getValue();
-                if (random <= 0) {
+            for (Map.Entry<String, Float> entry : pool.entrySet()) { //For every user, subtract the value of their entry.
+                randomval -= entry.getValue();
+                if (randomval <= 0) { //If after subtraction, the random number is 0 or less, their entry was picked, and they are a winner!
                     winner = entry.getKey();
                     chance = entry.getValue();
                     break;
                 }
             }
 
-            winners.add(new PlayerData(winner, chance));
-            pool.remove(winner);
+            winners.add(new PlayerData(winner, chance)); //Create a new player data object (stories their UUID and value) and add it to the list of winners
+            pool.remove(winner); //Remove the winner from the pool, as to not continue influencing the drawing with multiple pulls.
         }
     }
 
     public static void writeOutput() throws IOException {
+        //Create a writer for output file.
         BufferedWriter out;
         try {
             out = new BufferedWriter(new FileWriter("output.txt"));
@@ -194,8 +211,8 @@ public class Main {
         }
 
         int index = 0;
-        for (PlayerData winner : winners) {
-            String WinnerString = prizes.get(index) + ": " + winner.getUUID() + " (Entries: " + winner.getChance() + ")";
+        for (PlayerData winner : winners) { //For each winner, match the index of the winner to the index of the associated prize.
+            String WinnerString = prizes.get(index) + ": " + winner.getUUID() + " (Entries: " + winner.getChance() + ")"; //List the prize won, player who won it, and their entry count.
             out.write(WinnerString + "\n");
             System.out.println(WinnerString);
             index++;
